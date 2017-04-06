6add5c3960033cdbfc0ad3091a04d33918ca54b3
Kennedy <kennedys@miranetworks.net>
2017-04-06 16:24:02 +0200

    initial commit

---
+++ main.elm
@@ -0,0 +1,50 @@
+              
+               , onInput Password
+               , onInput Username
+               , type_ "password"
+               , type_ "text"
+               , value model.password 
+               , value model.username 
+               []
+               []
+               ]
+               ] 
+               else 
+        [ text model.message
+        ]
+        ]  
+        in
+        let 
+       [ text model.message
+      ({ model | message = str}, Cmd.none)
+      d = password}, Cmd.none)
+     ({ model | message = str}, Cmd.none)
+     ({ model | message = str}, Cmd.none)
+     ({ model | message = str}, Cmd.none) 
+     ({ model | passwor
+     ({ model | username = username }, Cmd.none)
+     loginPage model
+    , password = "123"
+    , password = "456"
+  
+     Filter str ->
+    Login str ->
+    loginPage model
+    Logout str ->
+    Password password ->
+    Reply str ->
+    Send str ->
+    Username  username ->
+    { init = init
+    { username : String
+    { username = "user1"
+    { username = "user2"
+    }
+    }
+    }
+    }
+   else
+  case msg 
+user1 =
+user2 : User
+user2 =
---
+++ me.elm
@@ -0,0 +1,68 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram
+        { model = initModel
+        , update = update
+        , view = view
+        }
+
+
+
+
+-- model
+
+
+type alias Model =
+    Int
+
+
+initModel : Model
+initModel =
+    0
+
+
+
+-- update
+
+
+type Msg
+    = AddCalorie
+    | Clear
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        AddCalorie ->
+            model + 1
+
+        Clear ->
+            initModel
+
+
+
+-- view
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h3 []
+            [ text ("Total Calories: " ++ (toString model)) ]
+        , button
+            [ type_ "button"
+            , onClick AddCalorie
+            ]
+            [ text "Add" ]
+        , button
+            [ type_ "button"
+            , onClick Clear
+            ]
+            [ text "Clear" ]
+        ]
